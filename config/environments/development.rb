9df5e31c7451873d528a7b17b890168c01d20acb
ahmed kucuk <ahmed.kucuk123@gmail.com>
2022-04-01 02:37:43 +0200

    ahmo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.assets.compile = true
+  config.serve_static_files = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
